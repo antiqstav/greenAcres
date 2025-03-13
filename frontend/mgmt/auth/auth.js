015af358c39881e596bab291838bc7eb3af17e11
antiq! <thatcommentaryguy159@gmail.com>
2025-03-13 00:18:45 -0500

    added login functionality

---
+++ frontend/mgmt/auth/auth.js
@@ -0,0 +1,81 @@
+// firebase initialization
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+import {
+  doc,
+  getDoc,
+  collection,
+  addDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBdtXeJeeT0ooE8yHsbMpUDVeXuDwkGAKk",
+  authDomain: "agrilife-96ce0.firebaseapp.com",
+  projectId: "agrilife-96ce0",
+  storageBucket: "agrilife-96ce0.firebasestorage.app",
+  messagingSenderId: "843694801338",
+  appId: "1:843694801338:web:eaac054371ac115316dd83",
+  measurementId: "G-Z9P0Q4RQ0H"
+};
+
+// firebase configuration
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// form handling
+const login = document.getElementById("login");
+const register = document.getElementById("register");
+
+// login logic
+document.getElementById("login").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = login.username.value;
+  const password = login.password.value;
+  const docname = username + "-" + password;
+
+  console.log(docname);
+
+  const docRef = doc(db, "crops", docname);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    // do something here to go to the next page
+    alert("Welcome, " + username);
+  }
+  else {
+    alert("Invalid username or password");
+    console.log("No such user with username (" + username + ")")
+  }
+
+  login.reset();
+});
+
+// register logic
+document.getElementById("register").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = register.username.value;
+  const password = register.password.value;
+  if (password.length < 8) {
+    alert("Password should be at least 8 characters long.");
+    // maybe add reference for password requirements?
+    return;
+  }
+  const docname = username + "-" + password;
+
+  console.log(docname);
+
+  const docRef = doc(db, "crops", docname);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    alert("User already exists with username " + username + ". Please try another username.");
+  }
+  else {
+    await setDoc(doc(db, "crops", docname), {
+      this: "worked!"
+    });
+    alert("User registered successfully. Please login to continue.");
+  }
+
+  register.reset();
+});
